b866c3ee175c3920f18446930a99ddb348af582e
Vattsal0306 <96181383+Vattsal0306@users.noreply.github.com>
2022-02-26 18:23:12 +0530

    Update Crypto Analysis.py
    This Just a Practice File for Data Analysis using Python and fill free add new more command in this file

--- Crypto Analysis.py
+++ Crypto Analysis.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[ ]:
-
-
-
-
-
-# In[34]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,55 +13,16 @@ prices['Bitcoin']=dfb["Price"]
 prices['Ethereaum']=dfe['Value']
 prices.plot(figsize=(16,9))
 null= prices.isnull()
-
-
-
-
-
-# In[24]:
-
-
 prices.fillna(method='bfill', inplace=True)
 prices.plot(figsize=(15,10))
-
-
-# In[32]:
-
-
 prices.plot(kind='hist', y='Ethereaum', bins=100)
-
-
-# In[38]:
-
-
 fig, ax = plt.subplots(figsize=(15, 7))
 sns.distplot(prices['Ethereaum'], ax=ax)
-
-
-# In[45]:
-
-
 fig, ax = plt.subplots(figsize=(15, 7))
 sns.distplot(prices['Bitcoin'], rug=True, ax=ax)
-
-
-# In[47]:
-
-
 fig, ax = plt.subplots(figsize=(15, 7))
-sns.distplot(prices['Bitcoin'], ax=ax,
-             hist_kws=dict(cumulative=True),
-             kde_kws=dict(cumulative=True))
-
-
-# In[54]:
-
-
+sns.distplot(prices['Bitcoin'], ax=ax, hist_kws=dict(cumulative=True), kde_kws=dict(cumulative=True))
 sns.jointplot(x="Bitcoin", y="Ethereaum", data=prices, size=5)
-
-
-# In[ ]:
-
 
 
 
